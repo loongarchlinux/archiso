50e2c3d91723264bb9aa536cb86323b5d79b22fe
Xiaotian Wu <wuxiaotian@loongson.cn>
2022-06-03 11:26:09 +0800

    mkarchiso: add loong64 support

--- configs/releng/profiledef.sh
+++ configs/releng/profiledef.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
+VER=${ID:-1}
 
 iso_name="archlinux"
-iso_label="ARCH_$(date +%Y%m)"
+iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="Arch Linux <https://archlinux.org>"
 iso_application="Arch Linux Live/Rescue CD"
-iso_version="$(date +%Y.%m.%d)"
+iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d).$VER"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
-           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
-arch="x86_64"
+bootmodes=('uefi-loong64.grub.esp'
+           'uefi-loong64.grub.eltorito')
+arch="loong64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
@@ -22,4 +22,5 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/usr/local/bin/setup"]="0:0:755"
 )

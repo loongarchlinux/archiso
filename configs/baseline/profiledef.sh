296979aeef72e2fb10088af40f0cd99170cdf4cf
Xiaotian Wu <wuxiaotian@loongson.cn>
2022-06-03 11:34:44 +0800

    baseline: profile

--- configs/baseline/profiledef.sh
+++ configs/baseline/profiledef.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux-baseline"
+ID=${ID:-1}
+
+iso_name="loongarchlinux-baseline"
 iso_label="ARCH_$(date +%Y%m)"
 iso_publisher="Arch Linux <https://archlinux.org>"
 iso_application="Arch Linux baseline"
-iso_version="$(date +%Y.%m.%d)"
+iso_version="$(date +%Y.%m.%d).$ID"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
-arch="x86_64"
+bootmodes=('uefi-la64.systemd-boot.esp' 'uefi-la64.systemd-boot.eltorito')
+arch="loongarch64"
 pacman_conf="pacman.conf"
-airootfs_image_type="erofs"
-airootfs_image_tool_options=('-zlz4hc,12')
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
 )

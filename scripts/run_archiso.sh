c73bf190d64838f3a277c63b5d13dfd297dd830e
Xiaotian Wu <wuxiaotian@loongson.cn>
2023-11-29 10:01:23 +0800

    add kvm for run_archiso.sh

--- scripts/run_archiso.sh
+++ scripts/run_archiso.sh
@@ -97,6 +97,7 @@ run_image() {
         -boot order=d,menu=on,reboot-timeout=5000 \
         -m "size=3072,slots=0,maxmem=$((3072*1024*1024))" \
         -k en-us \
+        -accel kvm \
         -cpu la464-loongarch-cpu \
         -name archiso,process=archiso_0 \
         -device virtio-scsi-pci,id=scsi0 \

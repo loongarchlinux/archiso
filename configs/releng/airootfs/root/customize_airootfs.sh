926f24104759e6a8ea8f43a292e766d984186dfe
Xiaotian Wu <wuxiaotian@loongson.cn>
2024-03-30 22:38:11 +0800

    locale

--- configs/releng/airootfs/root/customize_airootfs.sh
+++ configs/releng/airootfs/root/customize_airootfs.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env sh
-sed -i -e 's/^#zh_CN.UTF-8/zh_CN.UTF-8/' -e 's/^#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
+#sed -i -e 's/^#zh_CN.UTF-8/zh_CN.UTF-8/' -e 's/^#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
+cat >> /etc/locale.gen << __EOF__
+zh_CN.UTF-8 UTF-8
+en_US.UTF-8 UTF-8
+__EOF__
+
 /usr/bin/locale-gen
